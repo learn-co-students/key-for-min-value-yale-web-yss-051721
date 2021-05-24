0f8910359114862675c5d907691adc35ea98757a
Bill Feng <bill.feng@yale.edu>
2021-05-24 23:54:12 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,19 @@
 
 def key_for_min_value(name_hash)
 
+smallest=nil
+keysmol=nil
+
+name_hash.each do |key,value|
+  if(smallest==nil)
+    smallest=value
+    keysmol=key
+    next
+  end
+  if(value<smallest)
+    smallest=value
+    keysmol=key
+  end
+end
+keysmol
 end
