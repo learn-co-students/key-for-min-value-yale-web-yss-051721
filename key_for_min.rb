c104e70f13652e1560556d7bacd43915d519707e
Lindsay Chen <lindsay.chen@yale.edu>
2021-06-06 23:49:57 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,20 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+ if name_hash.length == 0 
+   nil 
+ else
+  smallest = ""
+  small = nil 
+  name_hash.each do |key, num|
+    if small == nil 
+    small = num
+    end 
+    if num < small
+      small = num
+    end
+    smallest = name_hash.index(small)
+  end
+smallest
+end
 end
